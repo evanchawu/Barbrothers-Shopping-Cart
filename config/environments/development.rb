861f17f484949a78af4f0a6b5af4b67c871f9cb1
evanchawu <evan.cha.wu@gmail.com>
2015-05-15 15:06:46 +0800

    try another way to set Braintree environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ end
 
 
 if Rails.env.production?
-  Braintree::Configuration.environment = Rails.env.staging? ? :sandbox : :production
+  Braintree::Configuration.environment = :production
   Braintree::Configuration.merchant_id = ENV["Settings.Braintree.MERCHANT_ID"]
   Braintree::Configuration.public_key  = ENV["Settings.Braintree.PUBLIC_KEY"]
   Braintree::Configuration.private_key = ENV["Settings.Braintree.PRIVATE_KEY"]
